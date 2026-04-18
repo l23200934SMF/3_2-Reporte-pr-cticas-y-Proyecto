21f091d9f3afd0b1ba435c3abf2d54b9a5d6d06c
Santiago Moya <l23200934@pachuca.tecnm.mx>
2026-04-17 22:56:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function mensaje(){
 alert("Benito Santos es una empresa de moda mexicana.");
 }
 
+function mostrar(){
+
+let productos = document.getElementById("productos");
+
+if(productos.style.display == "none"){
+productos.style.display = "block";
+}else{
+productos.style.display = "none";
+}
+
+}
